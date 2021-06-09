1c8dfdab1bff41f1848f7634bf1bce1e2b097222
Nahidjc <nahidjc80@gmail.com>
2021-06-10 00:25:43 +0600

    model incomplete

--- Cv_Maker_App/models.py
+++ Cv_Maker_App/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class CvMaker(models.Model):
+    fullname = models.CharField(
+        max_length=200, verbose_name='own name', blank=False)
+    email = models.EmailField(blank=False)
+    phone_number = models.CharField(max_length=12)
+    cv_picture = models.ImageField(upload_to='picture', blank=False)
+    website = models.URLField(blank=True)
+    present_adress = models.CharField(max_length=250, blank=False)
--- cv_maker_project/urls.py
+++ cv_maker_project/urls.py
@@ -1,11 +1,14 @@
+from Cv_Maker_App import views
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from Cv_Maker_App.views import home
 from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('Login_App.urls')),
-    # path('cv/', include('Cv_Maker_App.urls')),
+    path('', views.home, name='home'),
+    path('cv/', include('Cv_Maker_App.urls')),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
